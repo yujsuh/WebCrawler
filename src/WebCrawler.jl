4085f0ec20785a1568e32ca5618a8937466072fa
Yujung Suh <yujsuh75@gmail.com>
2022-04-27 10:14:42 +0900

    up

--- src/WebCrawler.jl
+++ src/WebCrawler.jl
@@ -1,5 +1,38 @@
 module WebCrawler
 
-# Write your package code here.
+using Dates
+
+# public interface
+export Target
+export add_site!, crawl_sites!, current_sites, reset_crawler!
+
+# == insert global variables and functins here ==
+mutable struct Target
+    url::String
+    finished::Bool = false
+    finsish_time::Union{DateTime,Nothing} = nothing
+end
+
+const sites = Target[]
+
+function add_site!(site::Target)
+    push!(sites, site)
+end
+
+function crawl_sites!()
+    for s in sites
+        index_site!(s)
+    end
+end
+
+function current_sites()
+    copy(sites)
+end
+
+function reset_crawler!()
+    empty!(sites)
+end
+
+
 
 end
